310c5c269f707482061262e81327f88de661bf2f
nhert <nathan.hertner@gmail.com>
2015-09-22 17:50:12 -0400

    First Test of the check for High Cards.

--- COMP4004-A1-POKER/src/Poker.java
+++ COMP4004-A1-POKER/src/Poker.java
@@ -1,10 +1,37 @@
 import java.util.ArrayList;
 
+/*Note of Rankings in Poker:
+ * High Card: 1
+ * One Pair: 2
+ * Two Pair: 3
+ * Three of a Kind: 4
+ * Straight: 5
+ * Flush: 6
+ * Full House: 7
+ * Four of a Kind: 8
+ * Straight Flush: 9
+ * Royal Flush: 10
+ */
+
 public class Poker {
 	
+	//logic: check for highest rank first then gradually move down until a match is found
 	public int getPokerRanking(){
-		
-		
+		//check rankings for all hands
+		//if there is a tie find high card
+		//if tied still then two winners, if not then output ranks of all the hands (by checking poker ranking)
 		return 0;
+	}
+	
+	//function for getting the high card in a hand
+	//note: this is not checked in the ranking func, if player doesn't at least have a pair they automatically have high card hand
+	public int GetHighCard(Hand h){
+		int currentHigh = 0;
+		for(int x = 0; x < 5; x++){
+			if(h.getCard(x).getRank() > currentHigh){
+				currentHigh = h.getCard(x).getRank();
+			}
+		}
+		return currentHigh;
 	}
 }
--- COMP4004-A1-POKER/src/TestPoker.java
+++ COMP4004-A1-POKER/src/TestPoker.java
@@ -11,5 +11,26 @@ public class TestPoker extends TestCase{
 		assertEquals(0, p.getPokerRanking());
 	}
 	
+	public void testHighCard(){
+		Poker p = new Poker();
+		Hand h = new Hand();
+		//regular cards
+		h.add(new Card(2, "Spades"));
+		h.add(new Card(3, "Spades"));
+		h.add(new Card(4, "Spades"));
+		h.add(new Card(5, "Spades"));
+		h.add(new Card(10, "Spades"));
+		assertEquals(10,p.GetHighCard(h));
+		
+		//some face cards
+		Hand h2 = new Hand();
+		h2.add(new Card(10, "Spades"));
+		h2.add(new Card(11, "Spades"));
+		h2.add(new Card(12, "Spades"));
+		h2.add(new Card(13, "Spades"));
+		h2.add(new Card(14, "Spades"));
+		assertEquals(14,p.GetHighCard(h2));
+	}
+	
 	
 }
